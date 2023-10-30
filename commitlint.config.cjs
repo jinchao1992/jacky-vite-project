9a22975c90139a92cd8d4412bc4147a7e974ebd8
jinchao1992 <jinchaophp@126.com>
2023-10-30 12:03:12 +0800

    feat: test commitlint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   rules: {
     'type-empty': [2, 'never'],
     'type-enum': [2, 'always', types],
-    'scope-case': [0, 'always'],
+    'scope-case': [2, 'never'],
     'subject-empty': [2, 'never'],
     'subject-case': [0, 'never'],
     'header-max-length': [2, 'always', 88]
